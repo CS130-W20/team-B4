20763bf006386866cc37a5bac11991b814ae7942
Sam Tang <samtang@wifi-131-179-53-190.host.ucla.edu>
2020-02-05 12:15:54 -0800

    setup routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,96 @@
 import React from 'react';
-import backgr from './img/freeimg.jpg';
+import { fireAuth } from "./fireApi";
+import Home from    "./Home";
+import Login from   "./Login";
+import Profile from "./Profile";
+import _ from "lodash";
+import Typography from "@material-ui/core/Typography";
+import withAuthProtection from "./withAuthProtection";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-	  <img src={backgr} className="Background-img" alt="logo" />
-    </div>
-  );
+
+const ProtectedProfile = withAuthProtection("/login")(Profile);
+
+class App extends React.Component {
+  constructor() {
+    super();
+    console.log("user", fireAuth.currentUser);
+    this.state = {
+      me: fireAuth.currentUser
+    };
+  }
+
+  componentDidMount() {
+    fireAuth.onAuthStateChanged(me => {
+      this.setState({ me });
+    });
+  }
+
+  handleSignIn = history => (email, password) => {
+    return fireAuth.signInWithEmailAndPassword(email, password).then(() => {
+      return history.push("/profile");
+    });
+  };
+
+  render() {
+    const { me } = this.state;
+    const email = _.get(me, "email");
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={() => (
+                <div>
+                <Link to="/login" style={{ marginRight: 16 }}>
+                  Login
+                </Link>
+                <Link to="/public" style={{ marginRight: 16 }}>
+                  Public
+                </Link>
+                <Link to="/profile">Profile</Link>
+                <Home />
+              </div>
+            )}
+          />
+          <Route
+            path="/login"
+            exact
+            render={({ history }) => (
+              <div>
+                <Link to="/">Home</Link>
+                <Login onSubmit={this.handleSignIn(history)} />
+              </div>
+            )}
+          />
+          <Route
+            path="/profile"
+            exact
+            render={props => (
+              <div>
+                <Link to="/">Home</Link>
+                <ProtectedProfile {...props} me={me} displayName={email} />
+              </div>
+            )}
+          />
+          <Route
+            path="/public"
+            exact
+            render={() => (
+              <div>
+                <Link to="/">Home</Link>
+                <Typography variant="h4">
+                  Public Route, anyone can see this page.
+                </Typography>
+              </div>
+            )}
+          />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/fireApi.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyBR_k_Mgjv2PG-b2rZVUQgEw8_6IFSwbLo",
+  authDomain: "adventum-625d1.firebaseapp.com",
+  projectId: "adventum-625d1"
+};
+
+const firebaseApp = firebase.initializeApp(config);
+
+export const fireAuth = firebaseApp.auth();
