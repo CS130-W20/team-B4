5523c29105aa723146a5bf3b9e274fcef2626d30
Julia Baylon <juliabaylon01@gmail.com>
2020-02-12 13:35:12 -0800

    cleaned up landing page, added new font (#10)

--- src/Landing.js
+++ src/Landing.js
@@ -10,10 +10,18 @@ class Landing extends React.Component {
     return (
       <div>
         <img src={backgr} className="Background-img" alt=""/>
-        <div  className="box pa7">
+        <div  className="box-landing pa7">
           <div className="title"> Adventum </div>
-            <div className="mv3 flex justify-center"><Button color={"primary"} variant={"contained"} component={Link} to={{pathname:'/login',state:{login:true}}} >Log In</Button></div>
-            <div className="mv3 flex justify-center"><Button variant={'contained'} component={Link} to={{pathname:'/login',state:{login:false}}} >Sign Up</Button></div>
+            <div className="mv3 flex justify-center">
+              <Button fullWidth size="large" color={"primary"} variant={"contained"} component={Link} to={{pathname:'/login',state:{login:true}}} >
+                Log In
+              </Button>
+            </div>
+            <div className="mv3 flex justify-center">
+              <Button fullWidth variant={'contained'} component={Link} to={{pathname:'/login',state:{login:false}}} >
+                Sign Up
+              </Button>
+            </div>
         </div>
       </div>
 
