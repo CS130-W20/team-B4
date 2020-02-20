a0a8daa8e4620e4d664278479925ad9c5f0540fd
Katherine Zhang <zhang.katherine14@gmail.com>
2020-02-20 13:49:39 -0800

    changed profile formatting to use percentages, added edit button

--- src/Profile.js
+++ src/Profile.js
@@ -3,10 +3,18 @@ import Popup from 'reactjs-popup';
 import './Profile.css';
 import { fireAuth } from "./fireApi";
 import Button from "@material-ui/core/Button";
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { lightBlue } from '@material-ui/core/colors';
+import blue from '@material-ui/core/colors/blue';
+import purple from '@material-ui/core/colors/purple';
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { withRouter } from 'react-router-dom';
 import clock from './img/clock.png';
 import road from './img/road.png';
 import price from './img/price.png';
 import maged from './img/maged.png';
+import edit from './img/edit.png';
+import logout from './img/arrow_white.png';
 
 import dots from './img/dots.png';
 
@@ -21,12 +29,37 @@ import kayak from './img/kayak.png';
 import chinese from './img/chinese.png';
 import hiking from './img/mountain.png';
 
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#ffffff',
+        },
+        secondary: {
+            main: '#2962ff',
+        }
+    },
+    overrides: {
+        MuiButton: {
+            label: {
+                color: '#ffffff',
+            },
+        }
+    }
+});
 
-export default class Profile extends Component{
+const style = {
+    textTransform: 'none',
+    borderRadius: '25px',
+    width: '150px',
+};
+
+class Profile extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            logged_in: false,
+            logged_in: props.location.state ? props.location.state.logged_in : false,
+            edit: props.location.state ? props.location.state.edit : false,
+            view: true, 
             name: "Maged Elaasar",
             username: "@maged_elaasar",
             blurb: "The first and third days after weekends are the hardest.",
@@ -46,24 +79,35 @@ export default class Profile extends Component{
             my_pref_map: []
         }
         for (var i = 0; i < this.state.my_prefs.length; i++) {
-        	this.state.my_pref_map.push({
-        		word: this.state.my_prefs[i],
-        		pic: this.state.pref_map[this.state.my_prefs[i]]
-        	});
+            this.state.my_pref_map.push({
+                word: this.state.my_prefs[i],
+                pic: this.state.pref_map[this.state.my_prefs[i]]
+            });
         }
+        this.state.view = !this.state.logged_in || !this.state.edit;
     }
 
     render() {
         const self = this;
         return (
             <div>
-                <div> <Topbar/> </div>
-                <div style={{marginLeft: '140px', position: 'absolute'}}> <LeftSide/> </div>  
-                <div style={{marginLeft: '700px', marginTop: '150px', position: 'absolute'}} className='fullname-display'> {this.state.name} </div>
-                <div style={{marginLeft: '700px', marginTop: '210px', position: 'absolute'}} className='username-display'> {this.state.username} </div>
-                <div style={{marginLeft: '700px', marginTop: '240px', position: 'absolute'}} className='blurb-display'> {this.state.blurb} </div>
-                <div style={{marginLeft: '700px', marginTop: '300px'}} className='username-display2'> {this.state.username} is down for... </div>
-                <div style={{marginLeft: '700px', position: 'absolute'}}> <RightSide my_prefs={this.state.my_prefs} pref_map={this.state.my_pref_map}/> </div>
+            <div> <Topbar/> </div>
+            {this.state.view ?
+            <div>
+                <div style={{marginLeft: '1%', marginTop: '4.5%', position: 'absolute'}} onClick={()=>{this.setState({'view': false})}}> <img src={edit} className='edit-img'/> </div>  
+                <div style={{marginLeft: '10%', position: 'absolute'}}> <LeftSide/> </div>  
+                <div style={{marginLeft: '48%', marginTop: '10%', position: 'absolute'}} className='fullname-display'> {this.state.name} </div>
+                <div style={{marginLeft: '48%', marginTop: '14%', position: 'absolute'}} className='username-display'> {this.state.username} </div>
+                <div style={{marginLeft: '48%', marginTop: '17%', position: 'absolute'}} className='blurb-display'> {this.state.blurb} </div>
+                <div style={{marginLeft: '48%', marginTop: '21%'}} className='username-display2'> {this.state.username} is down for... </div>
+                <div style={{marginLeft: '48%', marginTop: '24%', position: 'absolute'}}> <RightSide my_prefs={this.state.my_prefs} pref_map={this.state.my_pref_map}/> </div>
+            </div>
+            :
+            <div>
+            <div style={{marginTop: '42%', marginLeft: '45%', position: 'absolute'}} className="justify-center"> <ThemeProvider theme={theme}> <Button variant={"contained"} onClick={()=>{this.setState({'view': true})}} theme={theme} color={"secondary"} style={style}> Save </Button> </ThemeProvider> </div>
+            <div style={{marginTop: '45%', marginLeft: '46.5%', fontSize:15, textDecoration:'underline', position: 'absolute'}} className="message ph4 mt2" onClick={()=>{this.setState({view: 'true'})}}> cancel </div>
+            </div>
+            }
             </div>
         );
     }
@@ -74,21 +118,9 @@ class Topbar extends Component {
         return (
             <div>
                 <div className="topnav full-width-div">
-                    <div style={{float: 'right', margin: '10px'}}> <Button variant={"contained"} onClick={() => fireAuth.signOut()}> Log out </Button> </div>
-                    <div style={{float: 'right'}}> <img src={maged} className="thumbnail-img"/> </div>
+                    <div style={{float: 'right', marginRight: '2%', marginTop: '1%'}} onClick={() => fireAuth.signOut()}> <img src={logout} className='logout-img'/> </div>
+                    <div style={{float: 'right', marginRight: '2%', marginTop: '0.25%'}}> <img src={maged} className="thumbnail-img"/> </div>
                 </div>
-            </div>
-        );
-    }
-}
-
-class Text extends React.Component {
-    render() {
-        return (
-            <div>
-                <div className="fullname-display"> {this.props.name} </div>
-                <div style={{marginTop: '65px'}} className="username-display"> {this.props.username} </div>
-                <div style={{marginTop: '100px'}} className="blurb-display"> {this.props.blurb} </div>
             </div>
         );
     }
@@ -98,7 +130,7 @@ class LeftSide extends Component {
     render() {
         return (
             <div>
-                <div style={{marginTop: '150px'}} className='gradient-box'>
+                <div style={{marginTop: '45%'}} className='gradient-box'>
                 <div style={{marginTop: '30px', marginLeft: '50px', position: 'absolute'}}> <img src={maged} className="profile-img"/> </div>
                 <div style={{marginTop: '200px', marginLeft: '70px', position: 'absolute'}}> <LogisticalPreferences/> </div>
                 </div>
@@ -141,19 +173,19 @@ class RightSide extends Component {
         const manyPrefs = (my_prefs.length > 8);
         return (
             <div>
-                <div style={{marginTop: '340px'}} className='row'> {
-                    pref_map.slice(0,4).map( ({word, pic}) => { return <div key={pic} style={{marginRight: '65px'}}> {<img src={pic} className="activity-img"/>} <div className="drink"> {word} </div> </div> }) 
+                <div className='row'> {
+                    pref_map.slice(0,4).map( ({word, pic}) => { return <div key={pic} style={{marginRight: '65px'}}> {<img src={pic} className="activity-img"/>} <div className="label"> {word} </div> </div> }) 
                 } </div>
                 <div>
                     { manyPrefs ? (
                     <div style={{marginTop: '65px'}} className='row'>
-                    {pref_map.slice(4,7).map( ({word, pic}) => { return <div key={pic} style={{marginRight: '65px'}}> {<img src={pic} className="activity-img"/>} <div className="drink"> {word} </div> </div> })} 
+                    {pref_map.slice(4,7).map( ({word, pic}) => { return <div key={pic} style={{marginRight: '65px'}}> {<img src={pic} className="activity-img"/>} <div className="label"> {word} </div> </div> })} 
                     <Popup trigger={<img src={dots} className="dots"/>} position="right center">
                         <div> {pref_map.slice(7,pref_map.length).map( ({word, pic} ) => {return <div key={pic} className="tag-popup"> {word} </div>})} </div>
                     </Popup>
                     </div> ) : (
                     <div style={{marginTop: '65px'}} className='row'>
-                    {pref_map.slice(4,8).map( ({word, pic}) => { return <div key={pic} style={{marginRight: '65px'}}> {<img src={pic} className="activity-img"/>} <div className="drink"> {word} </div> </div> })} 
+                    {pref_map.slice(4,8).map( ({word, pic}) => { return <div key={pic} style={{marginRight: '65px'}}> {<img src={pic} className="activity-img"/>} <div className="label"> {word} </div> </div> })} 
                     </div>)}
                 </div>
             </div>
@@ -161,3 +193,4 @@ class RightSide extends Component {
     }
 }
 
+export default withRouter(Profile);
