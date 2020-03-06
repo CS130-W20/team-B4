bf6733d27c440d1d82d2c3349837979541671059
Evan <evancharfauros@gmail.com>
2020-03-06 01:50:03 -0800

    Progress on suggestion page layout

--- src/SuggestionSkeleton.js
+++ src/SuggestionSkeleton.js
@@ -3,7 +3,10 @@ import thai from "./img/thailand.png";
 import fourStars from "./img/yelp_stars/extra_large_4.png";
 import TopBar from './TopBar';
 import "./Suggestion.css";
+import edit from './img/edit.png';
+import background from './img/freeimg3.jpg';
 
+document.body.classList.add("no-sroll");
 
 export default class Suggestion extends Component{
     constructor(props){
@@ -16,18 +19,25 @@ export default class Suggestion extends Component{
     render(){
         return(
 
-            <div>
-                {/*<div><TopBar/></div>*/}
-                <div className="justify-center" style={{paddingTop: 100}}>
+            <div className="parent">
+                
+                <div><TopBar/></div>
+                <div style={{marginLeft: '1%', marginTop: '4.5%', position: 'absolute'}} 
+                    onClick={()=>{this.setState({'view': false})}}> <img src={edit} className='edit-img'/> 
+                </div>
+                <div className="justify-center" style={{paddingTop: 110, zIndex: 10}}>
                     <img className="suggestion-image" src={thai} alt="" width="300" height="300" />
                     {/*TODO: change image to match desired location*/}
                 </div>
                 <div className="suggestion-name">Emporium Thai</div>
                 {/*TODO: make this text field match the desired location name"*/}
-                <div className="flex justify-center">
-                    <img src={fourStars} alt="star rating"></img> {/*TODO: choose sprite based on rating*/}
-                    <div>$$</div> {/*TODO: num dollar signs should match destination price*/}
+                <div className="flex justify-center" style={{alignItems: "center"}}>
+                    <img src={fourStars} alt="star rating" style={{maxHeight: 30, paddingRight: 30}}></img> {/*TODO: choose sprite based on rating*/}
+                    <div style={{fontWeight: "bold", fontFamily: "Raleway", fontSize: 35}}>$$</div> 
+                    {/*TODO: num dollar signs should match destination price*/}
+                    <div style={{fontWeight: "bold", fontFamily: "Raleway", fontSize: 30, paddingLeft: 30}}>Thai Restaurant</div>
                 </div>
+                <img src={background} className="background"></img>
             </div>
         )
     }
