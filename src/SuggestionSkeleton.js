9b2720fe86fc75a9e1997d0eeeab0bc881fc8672
Evan <evancharfauros@gmail.com>
2020-03-06 03:42:33 -0800

    Designed Yelp box on Suggestion page

--- src/SuggestionSkeleton.js
+++ src/SuggestionSkeleton.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import thai from "./img/thailand.png";
 import fourStars from "./img/yelp_stars/extra_large_4.png";
 import TopBar from './TopBar';
+import yelpLogo from "./img/Yelp_Logo.png";
 import "./Suggestion.css";
 import edit from './img/edit.png';
 import background from './img/freeimg3.jpg';
@@ -23,14 +24,21 @@ export default class Suggestion extends Component {
                     <div style={{ marginLeft: '1%', marginTop: '4.5%', position: 'absolute' }}
                         onClick={() => { this.setState({ 'view': false }) }}> <img src={edit} className='edit-img' />
                     </div>
-                    <div className="justify-center" style={{ paddingTop: 20}}>
-                        <img className="suggestion-image" src={thai} alt="" width="300" height="300" />
+                    <div className="justify-center" style={{ paddingTop: 20 }}>
+                        <img className="suggestion-image" src={thai} alt="" width="260" height="260" />
                         {/*TODO: change image to match desired location*/}
                     </div>
                     <div className="suggestion-name">Emporium Thai</div>
                     {/*TODO: make this text field match the desired location name"*/}
                     <div className="flex justify-center" style={{ alignItems: "center" }}>
-                        <img src={fourStars} alt="star rating" style={{ maxHeight: 30, paddingRight: 30 }}></img> {/*TODO: choose sprite based on rating*/}
+                        <div className="yelp-box pa3 pt3">
+                        {/*TODO: make yelp box a button that links to Yelp, modify contents of box to be accurate*/}
+                            <img src={fourStars} alt="star rating"></img>
+                            <div className="flex justify-center" style={{alignItems: "center", paddingTop: 5}}>
+                                <img src={yelpLogo} alt="yelp" style={{width: "40%", paddingLeft: 10}}></img>
+                                <div style={{ fontFamily: "Raleway", fontSize: 20, color: "white"}}>106 reviews</div>
+                            </div>
+                        </div>
                         <div style={{ fontWeight: "bold", fontFamily: "Raleway", fontSize: 35 }}>$$</div>
                         {/*TODO: num dollar signs should match destination price*/}
                         <div style={{ fontWeight: "bold", fontFamily: "Raleway", fontSize: 30, paddingLeft: 30 }}>Thai Restaurant</div>
