bd3de8270558a4846d8eda8fe70926b090f65920
JuliaB1 <juliabaylon01@gmail.com>
2020-03-11 00:15:40 -0700

    added price considerations

--- src/Home.js
+++ src/Home.js
@@ -74,7 +74,7 @@ const style = {
 
 
 /**
- *    [WIP] Each added user will receive a Card component to represent them in a session.
+ *    Each added user will receive a Card component to represent them in a session.
  */
 export default class Home extends Component{
 
@@ -94,40 +94,101 @@ export default class Home extends Component{
               showSuggestion: false, // if true, a query has been made
               showProfile: false,
               modify: false,
-              tempPrefs: {}, // save a copy of each person's preferences to modify on the fly
+              tempPrefs: {}, // save a copy of each person's activity preferences to modify on the fly
+              pricePrefs: {}, // save a copy of each person's high price preferences to modify on the fly
           }
       }
 
     /** Queries businesses around UCLA that match the specified category
      *
+     *  @precondition categories needs to be array of our tags to be used in search
+                      (returned by getCategoryListFromMap)
      *  @param categories array of places (strings) to include in filter
      *                    note: ["hiking","chinese"] => hiking OR chinese
      *  @return promise for yelp API call
      */
-    getLocation = categories => {
-      // Convert our category labels to those used by Yelp
+    getLocation = (categories, price) => {
+      // Convert our activity category labels to those used by Yelp
       var yelpCategories = [];
       categories.forEach((category) => {yelpCategories = yelpCategories.concat(yelpMap[`${category}`])})
 
-      // Convert to REST API format (comma delineated list)
+      // Convert activity category labels to REST API format (comma delineated list)
       var categoryString = "";
       if(yelpCategories.length != 0) {
         yelpCategories.forEach((x) => {categoryString += x + ","});
         categoryString = categoryString.substring(0, categoryString.length-1);
       }
+
+      var priceString = "1,2,3,4";
+      if(price.length != 0) {
+        priceString = "";
+        price.forEach((x) => {priceString += x + ","});
+        priceString = priceString.substring(0, priceString.length-1);
+      }
+
+      console.log(categoryString);
+      console.log(priceString);
+
       var yelpCall = firebase.functions().httpsCallable('yelpCall');
       console.log(categoryString);
       return yelpCall({ params: {
                       categories: `${categoryString}`,
+                      price: `${priceString}`,
                       latitude: 34.0689,
                       longitude: -118.4452,}});
      }
 
+     getPriceFromPrefs = () => {
+       var temp = {
+         1: 0,
+         2: 0,
+         3: 0,
+         4: 0
+       };
+       var numParticipants = 0;
+       this.state.all.forEach((u)=>{
+                   if(this.state.display[u.username]) {
+                       numParticipants++;
+                       console.log(this.state.pricePrefs[u.username]);
+                       if(this.state.pricePrefs[u.username] >= 61)
+                          temp[4]++;
+                       else if(this.state.pricePrefs[u.username] >= 60)
+                          temp[3]++;
+                       else if(this.state.pricePrefs[u.username] >= 30)
+                          temp[2]++;
+                       else
+                          temp[1]++;
+                   }});
+
+      var ret = [];
+      // If most people want a price point, add that
+      for(var key in temp) {
+        if(temp[key] >= numParticipants) {
+            ret.push(key);
+            return ret;
+        }
+      }
+
+      // Otherwise, just consider all preferences
+      for(var key in temp) {
+        if(temp[key] != 0)
+          ret.push(key);
+      }
+      return ret;
+     }
+
+    /**
+     *  Counts everyone's activity preferences
+     *
+     *  @return array of tags to be used by getLocation function for a Yelp query
+     */
     getCategoryListFromMap = () => {
       // Count preferences from tempPrefs
       var temp = {};
+      var numParticipants = 0;
         this.state.all.forEach((u)=>{
                     if(this.state.display[u.username]) {
+                      numParticipants++;
                         this.state.tempPrefs[u.username].forEach((pref) => {
                           if(temp[`${pref}`] == undefined) {
                             temp[`${pref}`] = 1;
@@ -139,10 +200,19 @@ export default class Home extends Component{
                     }});
 
       var ret = [];
+
       for(var key in temp) {
-        // If at least 2 people want an activity, include in search
-        if(temp[key] >= 2)
+        // If everyone wants an activity, search for that
+        if(temp[key] >= numParticipants)
           ret.push(key);
+      }
+
+      if(ret.length == 0) {
+        for(var key in temp) {
+          // If empty...and at least 2 people want an activity, include in search
+          if(temp[key] >= 2)
+            ret.push(key);
+        }
       }
 
       // If empty, everyone has separate preferences, so consider everything
@@ -154,6 +224,10 @@ export default class Home extends Component{
       return ret;
     }
 
+    /**
+     *  @param card card to exclude from search session
+     *
+     */
     handleCardDelete = (card) => {
         var s = this.state.display;
         s[card.username] = false;
@@ -162,6 +236,9 @@ export default class Home extends Component{
       // this.setState({all: newAll});
     }
 
+    /**
+     * @param e keyboard event, processes search bar input
+     */
     handleSearchBar = (e) => {
         if(!this.state.modify){
             if(e.key === 'Escape'){
@@ -180,10 +257,18 @@ export default class Home extends Component{
         }
 
     }
+
+    /**
+     * @param e used simply to detect a click event (disables search bar when)
+     *          temporarily modifying logistical preferences
+     */
     handleModify = e => {
         this.setState({modify: !this.state.modify});
     }
 
+    /**
+     * @param user include specified user card in activity search
+     */
     addCard = (user)=>{
         var s = this.state.display;
         s[user.username]=true;
@@ -191,6 +276,9 @@ export default class Home extends Component{
 
     }
 
+    /**
+     * @param event key event, used to detect when user clicks out of the searchbar
+     */
     targetHasClickHandler = (event) => {
         let el = event.target;
         while(el) {
@@ -217,11 +305,13 @@ export default class Home extends Component{
                 this.setState({all:users});
                 var s = {};
                 var prefs = {};
+                var pricePrefs = {};
                 this.state.all.forEach((user)=> {
                   s[user.username]=false;
                   prefs[user.username] = user.preferences;
+                  pricePrefs[user.username] = user.high;
                 });
-                this.setState({display:s, tempPrefs: prefs});
+                this.setState({display:s, tempPrefs: prefs, pricePrefs: pricePrefs});
             })
 
         // var l = [];
@@ -245,14 +335,20 @@ export default class Home extends Component{
         var l = [];
         this.state.all.forEach((u)=>{
             if(this.state.display[u.username]) {
-                l.push(<Card handleModify={this.handleModify} key={u.username} updateHomePrefs={this.updatePrefs}  data={u} curPrefs={this.state.tempPrefs[u.username]} imgURL = {u.pic ? this.getURL(u.pic) : ''} deleteCard = {this.handleCardDelete}/>);
+                l.push(<Card handleModify={this.handleModify} key={u.username} updateHomePrefs={this.updatePrefs}  data={u} curPrefs={this.state.tempPrefs[u.username]} curPricePrefs={this.state.pricePrefs[u.username]} imgURL = {u.pic ? this.getURL(u.pic) : ''} deleteCard = {this.handleCardDelete}/>);
             }});
             return l;
     }
 
+    /**
+     * Make Yelp query!
+     */
     makeQuery = () => {
-      var list = this.getCategoryListFromMap();
-      this.getLocation(this.getCategoryListFromMap()).then((response) =>{
+      var activityPrefs = this.getCategoryListFromMap();
+      var pricePrefs = this.getPriceFromPrefs();
+      console.log("here!!");
+      console.log(pricePrefs);
+      this.getLocation(activityPrefs, pricePrefs).then((response) =>{
         this.setState({
           queryResult:response.data.businesses
         })
@@ -269,11 +365,26 @@ export default class Home extends Component{
       })
     }
 
-    updatePrefs = (user, preferences) => {
-      var curPrefs = this.state.tempPrefs;
-      curPrefs[user] = preferences;
-      console.log(curPrefs);
-      this.setState({tempPrefs: curPrefs});
+    /**
+     * When temporarily modifying preferences
+     *
+     * @param user username to update activity preferences for
+     * @param preferences activity preferences to update to
+     * @param thingToUpdate must be 'activities', or 'price'
+     */
+    updatePrefs = (user, preferences, thingToUpdate) => {
+      console.log(user);
+      if(thingToUpdate == 'activities') {
+          var curPrefs = this.state.tempPrefs;
+          curPrefs[user] = preferences;
+          this.setState({tempPrefs: curPrefs});
+      }
+      else if (thingToUpdate == 'price') {
+        var curPrefs = this.state.pricePrefs;
+        curPrefs[user] = preferences;
+        console.log(curPrefs);
+        this.setState({pricePrefs: curPrefs});
+      }
     }
 
     render(){
